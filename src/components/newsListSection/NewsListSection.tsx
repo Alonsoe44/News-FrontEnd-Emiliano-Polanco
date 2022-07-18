a2e6592f1902adb491620cbaad86881b819dd308
Emiliano Polanco Baquerizo <emilianopolanco5@gmail.com>
2022-07-18 16:43:54 +0200

    Reading news from the backend

--- src/components/newsListSection/NewsListSection.tsx
+++ src/components/newsListSection/NewsListSection.tsx
@@ -1,28 +1,29 @@
+import { useEffect, useState } from "react";
 import NewsInterface from "../../interfaces/NewsInterface";
 import NewsCard from "../newsCard/NewsCard";
 import SectionButton from "./sectionButton/SectionButton";
 import SectionTitle from "./sectionTitle/SectionTitle";
+import axios from "axios";
 
-const myAmazingNew: NewsInterface = {
-  title:
-    "Here today we bought all the bitcon in the market, Allfunds it's goinc all in with cryptocurrency",
-  description:
-    "Cryptocurrency may be a good investment if you are willing to accept it is a high risk gamble which could pay off – but also that there is a strong chance you could lose all of your money. Prices of cryptocurrencies including bitcoin have been falling in 2022 amid a worldwide crypto price crash.",
-  date: "2020-01-01",
-  author: "John Doe",
-  content: "Lorem ipsum",
-};
-
+const apiUrl = process.env.REACT_APP_API_URL;
 const NewsListSection = () => {
+  const [news, setNews] = useState<NewsInterface[]>([]);
+  useEffect(() => {
+    axios.get((apiUrl + "/news") as string).then((res) => {
+      setNews(res.data);
+    });
+  }, []);
   return (
-    <article className="h-screen p-6 ">
+    <article className="p-6 ">
       <SectionTitle text="Look at our latest news" />
       <SectionButton
         action={() => {
           console.log("elpepe");
         }}
       />
-      <NewsCard newsData={myAmazingNew} />
+      {news.map((news) => (
+        <NewsCard newsData={news} key={news._id} />
+      ))}
     </article>
   );
 };
